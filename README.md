e8e11a03f8cf40059455018a21376ba89b965ed6
ximet <dq.ximet@gmail.com>
2017-11-20 20:00:22 +0300

    complete with simple example

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ How start:
 2) run bash script: sh build.sh
 3) run bash script: sh run.sh
 4) open VIM and write some C code 
-5) run command in docker: emcc YOUR_FILE.c -s WASM=1 -s SIDE_MODULE=1 -o YOUR_FILE.wasm
+5) run command in docker: emcc YOUR_FILE.c -O1 -o YOUR_FILE.wasm -s WASM=1 -s SIDE_MODULE=1
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import Counter from '../source/counter.wasm'  
 const wasmHelloWorld = () => {  
-    const counter = new Counter();
+    const counter = new Counter({
+        'env': {
+          'memoryBase': 0,
+          'tableBase': 0,
+          'memory': new WebAssembly.Memory({initial: 256}),
+          'table': new WebAssembly.Table({initial: 0, element: 'anyfunc'})
+        }
+      });
 
     counter
-        .then(result => console.log(result))
+        .then(module => {
+            console.log('Counter example', module.instance.exports._count());
+        })
         .catch(error => console.log(error))
 }
 window.onload = wasmHelloWorld  
